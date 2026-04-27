1933689f623b55b3629005da70dda76fba6f1dcf
MorreuMamandor <miguellessa08@gmail.com>
2026-04-26 22:14:25 -0300

    Pequenas alterações na navbar, reformulação do form de cadastro, criação do form da pagina de login, inicio da pagina de profissionais

--- fast-service/js/home-page.js
+++ fast-service/js/home-page.js
@@ -10,15 +10,3 @@ function scrollCarrossel(direcao) {
   const carrossel = document.getElementById("carrossel");
   carrossel.scrollLeft += direcao * 250;
 }
-const userIcon = document.getElementById("userIcon");
-const userMenu = document.getElementById("userMenu");
-
-userIcon.addEventListener("click", (e) => {
-  e.stopPropagation();
-  userMenu.style.display =
-    userMenu.style.display === "flex" ? "none" : "flex";
-});
-
-document.addEventListener("click", () => {
-  userMenu.style.display = "none";
-});
